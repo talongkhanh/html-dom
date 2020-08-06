15b44cda505a2c1abf1c72faa8568efe062fdff1
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-06 12:48:49 +0700

    getElementsByTagName method

--- 01-get-element-method/main.js
+++ 01-get-element-method/main.js
@@ -4,3 +4,7 @@ console.log(paragraphNode);
 var headingNode = document.getElementsByClassName('heading');
 // getElementsByClassName return HTML collection like array
 console.log(headingNode[0]);
+
+var menuNode = document.getElementsByTagName('ul');
+// getElementsByTagName return HTML collection like array
+console.log(menuNode);
