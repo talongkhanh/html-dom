896fa5a491907a705b86698858f6f7b23cc8d81d
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-06 12:54:09 +0700

    Css Selector

--- 01-get-element-method/main.js
+++ 01-get-element-method/main.js
@@ -7,4 +7,17 @@ console.log(headingNode[0]);
 
 var menuNode = document.getElementsByTagName('ul');
 // getElementsByTagName return HTML collection like array
-console.log(menuNode);
+console.log(menuNode[0]);
+
+
+//querySelector return one element (first element)
+var itemNode = document.querySelector('.list-item');
+console.log(itemNode);
+
+// querySelectorAll return NodeList like array
+var itemNodes = document.querySelectorAll('.list-item');
+console.log(itemNodes);
+
+for(var itemNode of itemNodes) {
+	console.log(itemNode);
+}
