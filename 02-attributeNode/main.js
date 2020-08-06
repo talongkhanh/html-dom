b51b697dff6902b5065fb1b9a4281794bf35cfb9
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-06 13:05:23 +0700

    DOM attribute

---
+++ 02-attributeNode/main.js
@@ -0,0 +1,6 @@
+var headingNode = document.querySelector('.heading');
+console.log(headingNode);
+
+headingNode.title = 'Heading';
+headingNode.setAttribute('demo', 'demo')
+console.log(headingNode.attributes)
