d5221ca60028eaedacabd609fbf3378c51e2d39c
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-06 12:46:58 +0700

    getElementsByClassName method

--- 01-get-element-method/main.js
+++ 01-get-element-method/main.js
@@ -1,2 +1,6 @@
 var paragraphNode = document.getElementById('paragraph-1');
 console.log(paragraphNode);
+
+var headingNode = document.getElementsByClassName('heading');
+// getElementsByClassName return HTML collection like array
+console.log(headingNode[0]);
